352276ec32ef7b10521093356a1e37d62540d36f
Pedro Nunes <nunespedronunes@yahoo.com.br>
2023-08-17 19:24:48 -0300

    req 4 na branch principal do grupo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+import { getCategories } from '../services/api';
+
+type PegaCategoria = {
+  id: string,
+  name: string,
+};
+
 function Home() {
+  const [categorias, setCategorias] = useState<PegaCategoria[]>([]);
+
+  useEffect(() => {
+    const pegaApi = async () => {
+      const data = await getCategories();
+      setCategorias(data);
+    };
+    pegaApi();
+  }, []);
   return (
     <div>
-      <input
-        type="text"
-      />
+      { categorias.map((categoria) => (
+        <label
+          data-testid="category"
+          key={ categoria.id }
+          htmlFor=""
+        >
+          <input
+            type="radio"
+          />
+          { categoria.name }
+        </label>
+      )) }
       <p
         data-testid="home-initial-message"
       >
