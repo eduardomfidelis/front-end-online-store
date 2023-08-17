241d5213d9a87b154f0a773bbc21455c2d0282de
eduardo maciel <eduardomacielfidelis@gmail.com>
2023-08-17 17:18:23 -0300

    requisito(2)Crie uma página de listagem de produtos vazia

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <input
+        type="text"
+      />
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    </div>
+  );
+}
+export default Home;
